684f6e87c6de86b7be0c56c3d95a80e419e40577
pablo <rezpablo@gmail.com>
2018-02-02 02:23:48 -0200

    first commit

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { Switch, Route } from 'react-router-dom'
+
+const Main = () => (
+    <main>
+        <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/roster' component={Roster}/>
+            <Route path='/schedule' component={Schedule}/>
+        </Switch>
+    </main>
+
+export default App;
