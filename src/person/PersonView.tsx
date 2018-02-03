77620e5333b5321c69f8b63ff0534366ffbd2b84
pablo <rezpablo@gmail.com>
2018-02-03 00:12:21 -0200

    Adicionando tabela

--- src/person/PersonView.tsx
+++ src/person/PersonView.tsx
@@ -2,11 +2,8 @@ import {Dispatch} from 'redux';
 import {connect} from 'react-redux';
 import * as React from 'react';
 
-import {model} from './index';
-import PersonItem from './components/PersonItem';
-import {Card} from 'primereact/components/card/Card';
-import {DataTable} from 'primereact/components/datatable/DataTable';
-import {Column} from 'primereact/components/column/Column';
+import {model} from './index'
+import PersonItem from "./components/PersonItem";
 
 interface PersonViewProps {
     person: model.Person[];
@@ -18,27 +15,17 @@ class PersonView extends React.Component<PersonViewProps> {
     render() {
         const { person } = this.props;
 
-         return (
-            <Card>
-                <div>
-
-                    <DataTable value={person}>
-                        <Column field="id" header="#" />
-                        <Column field="name" header="Nome" />
-                        <Column field="surname" header="Sobrenome" />
-                    </DataTable>
-
-
-                    <h1>
-                        People
-                    </h1>
-                    <ul>
-                        {person.map(p =>
-                            <PersonItem person={p} key={p.id}/>
-                        )}
-                    </ul>
-                </div>
-            </Card>
+        return (
+            <div>
+                <h1>
+                    People
+                </h1>
+                <ul>
+                    {person.map(p =>
+                        <PersonItem person={p}/>
+                    )}
+                </ul>
+            </div>
         );
     };
 }
